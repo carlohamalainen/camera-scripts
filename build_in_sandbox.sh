4be4a40c70a0bb9caf1512ec2c279b31a55af4fd
Carlo Hamalainen <carlo@carlo-hamalainen.net>
2014-03-03 09:49:12 +0100

    cabal constraints

--- build_in_sandbox.sh
+++ build_in_sandbox.sh
@@ -2,12 +2,11 @@
 
 PS4='($LINENO)+ '
 set -x
+set -e
 
 rm -fr .cabal-sandbox cabal.sandbox.config dist
 
 cabal sandbox init
-
-export PATH=`pwd`/.cabal-sandbox/bin:$PATH
 
 cabal install --haddock-hyperlink-source --dependencies-only # Is this necessary? Why not just cabal install?
 cabal install
